2051b10ee7bb2f6d624435f32e6bc8ad0d03111d
nour <nour@beamng.com>
2021-02-02 12:03:56 +0100

    api-auth

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -14,8 +14,12 @@ class LoginController extends Controller
             'email'=>'required|string',
             'password'=>'required|string'
         ]);
+
         if (!Auth::attempt($login)){
             return response(['message=>Invalid login credntials.']);
         }
+        // Creating a token without scopes...
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+        return  response(['user'=>Auth::user(),'access_token'=>$accessToken]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('/user')->group(function(){
+    Route::post('/login','API\LoginController@login');
+    Route::middleware('auth:api')->get('/user', 'API\UserController@index');
 });
